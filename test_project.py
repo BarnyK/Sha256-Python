ed91fa9501a6cec67bc3f6d6351b879fe5501bfc
Barnaba Krupowicz <barnaba.krupowicz@gmail.com>
2020-12-21 23:01:29 +0100

    Padding and tests

--- test_project.py
+++ test_project.py
@@ -1,10 +1,40 @@
-from main import encrypt
 import unittest
-from hashlib import sha256
 
 
-class Test_Encrypt(unittest.TestCase):
-    def test_correct_encryption(self):
+class Test_Encryption(unittest.TestCase):
+
+    def setUp(self):
+        from main import encrypt
+        self.encrypt = encrypt
+
+    def test_result_against_hashlib(self):
+        from hashlib import sha256
+        message = "mzxkjcvnjkznxjkgvneqwiufhqwpqmajsdklfmaklsdmflkmklni2h543245njk"
+        encrypted_test = self.encrypt(message)
+        encrypted_known = sha256(message)
         self.assertEqual(
-            encrypt("abcd".encode()).digest(), sha256("abcd".encode()).digest()
+            encrypted_test, encrypted_known
         )
+class Test_Padding(unittest.TestCase):
+
+    def setUp(self):
+        from main import pad_message
+        self.pad_message = pad_message
+
+    def test_correct_padding(self):
+        message = b'abcd'
+        padded_message = self.pad_message(message)
+        message += b'\x80' + b'\x00' * 51
+        self.assertEqual(padded_message, message)
+
+    def test_correct_padding_for_empty(self):
+        message = b""
+        padded_message = self.pad_message(message)
+        message = b'\x80' + b'\x00' * 55
+        self.assertEqual(padded_message, message)
+
+    def test_correct_padding_for_big(self):
+        message = b"a" * 534
+        padded_message = self.pad_message(message)
+        message += b'\x80' + b'\x00' * 33
+        self.assertEqual(padded_message, message)
